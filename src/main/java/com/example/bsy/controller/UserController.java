b7dd18099ec7c503b5801353a8d199d8b08ae22d
bagsunyoung <malang333@naver.com>
2025-10-03 19:10:15 +0900

    회원가입 엔드포인트 추가

--- src/main/java/com/example/bsy/controller/UserController.java
+++ src/main/java/com/example/bsy/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.bsy.entity.User;
 import com.example.bsy.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("/register")
     public ResponseEntity<User> signUp(@RequestBody RegisterRequestDto registerRequestDto){
         User user = userService.register(registerRequestDto);
         return ResponseEntity.ok(user);
