a69fc62704a199e92a501940ebba20768f33e09e
bagsunyoung <malang333@naver.com>
2025-10-04 00:48:36 +0900

    초기 데이터 생성시각 추가 적용

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,6 @@
 USE bsy;
 
 INSERT INTO users (username, email, password, created_at) VALUES
-('김철수', 'kim098@example.com', 'cs123', ),
-('박영희', 'park123@example.com', 'abcd098'),
-('최민지', 'choi5656@example.com', 'minji77');
+('김철수', 'kim098@example.com', 'cs123', CURRENT_TIMESTAMP),
+('박영희', 'park123@example.com', 'abcd098', CURRENT_TIMESTAMP),
+('최민지', 'choi5656@example.com', 'minji77', CURRENT_TIMESTAMP);
