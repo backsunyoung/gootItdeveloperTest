ba58985f0703febbba58671a381ba251f23e8995
bagsunyoung <malang333@naver.com>
2025-10-04 00:44:31 +0900

    h2에서 mysql 전용 서버로 적용하고 초기 데이터 추가

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,6 @@
+USE bsy;
+
+INSERT INTO users (username, email, password, created_at) VALUES
+('김철수', 'kim098@example.com', 'cs123', ),
+('박영희', 'park123@example.com', 'abcd098'),
+('최민지', 'choi5656@example.com', 'minji77');
